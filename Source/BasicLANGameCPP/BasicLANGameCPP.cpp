cd8af701741e1d651de4f6c9b4d1e646fc8fdb1f
inoble6502 <inoble6502@gmail.com>
2021-08-01 01:29:31 -0400

    first commit

---
+++ Source/BasicLANGameCPP/BasicLANGameCPP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BasicLANGameCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasicLANGameCPP, "BasicLANGameCPP" );
---
+++ Source/BasicLANGameCPP/BasicLANGameCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasicLANGameCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICLANGAMECPP_API ABasicLANGameCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
