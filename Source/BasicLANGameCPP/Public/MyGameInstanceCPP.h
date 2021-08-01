001b11127bc31e03988e17b41ab1025852eada70
inoble6502 <inoble6502@gmail.com>
2021-08-01 01:54:22 -0400

    MyGameInstanceCPP creation

---
+++ Source/BasicLANGameCPP/Public/MyGameInstanceCPP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstanceCPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICLANGAMECPP_API UMyGameInstanceCPP : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
